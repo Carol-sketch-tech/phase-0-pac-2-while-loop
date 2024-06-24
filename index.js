606929bce2e5631c1a54944494b08e58d2b91444
Caroline <bancroftwanjiru10000@gmail.com>
2024-06-24 21:36:45 +0300

    inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// while loops 
+// basic syntax for while loops 
+// while (condition expression){
+    // sutff to be done . note that as long as the condition in 
+    // expression is truthy the code inside will run.
+// }
+// while (-1)
+//    { console.log("say  this is forever....")}
+// note that the conditional expression -1 is truthy therefore will result in an infifnity while loop
+// terminating a infinity loop 
+let count = 0;
+while (count < 8){
+console.log('I am the count,i love to count!')
+count =count + 1;
+}
